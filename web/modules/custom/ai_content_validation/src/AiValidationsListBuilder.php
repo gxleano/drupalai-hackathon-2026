6c24b264b36244e47a05e76887329c03dd6d66a2
Enrique Lacoma Pérez <enriquelacomaperez@gmail.com>
2026-01-28 09:17:24 +0100

    Add columns to valiations list

--- web/modules/custom/ai_content_validation/src/AiValidationsListBuilder.php
+++ web/modules/custom/ai_content_validation/src/AiValidationsListBuilder.php
@@ -17,8 +17,9 @@ final class AiValidationsListBuilder extends EntityListBuilder {
    */
   public function buildHeader(): array {
     $header['id'] = $this->t('ID');
-    $header['label'] = $this->t('Label');
-    $header['status'] = $this->t('Status');
+    $header['workflow'] = $this->t('Workflow');
+    $header['label'] = $this->t('Validation');
+    $header['validation_status'] = $this->t('Validation Status');
     $header['uid'] = $this->t('Author');
     $header['created'] = $this->t('Created');
     $header['changed'] = $this->t('Updated');
@@ -31,8 +32,16 @@ final class AiValidationsListBuilder extends EntityListBuilder {
   public function buildRow(EntityInterface $entity): array {
     /** @var \Drupal\ai_content_validation\AiValidationsInterface $entity */
     $row['id'] = $entity->id();
+    $workflow = $entity->get('field_flowdrop_workflow')->getValue();
+    $flowdrop_id = $workflow[0]['target_id'] ?? '';
+    $workflow_label = "";
+    if ($flowdrop_id) {
+      $flowdrop = \Drupal::entityTypeManager()->getStorage('flowdrop_workflow')?->load($flowdrop_id);
+      $workflow_label = $flowdrop->label();
+    }
+    $row['workflow'] = $workflow_label;
     $row['label'] = $entity->toLink();
-    $row['status'] = $entity->get('status')->value ? $this->t('Enabled') : $this->t('Disabled');
+    $row['validation_status'] = $entity->get('field_validation_status')->value;
     $username_options = [
       'label' => 'hidden',
       'settings' => ['link' => $entity->get('uid')->entity->isAuthenticated()],
